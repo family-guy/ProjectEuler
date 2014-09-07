24f5c777f6bb064c177d30ccd8a4c8791a49f173
family-guy <grking8@gmail.com>
2014-09-07 12:31:59 +0100

    Add problem27/IntegerArithmetic.java

--- problem27/IntegerArithmetic.java
+++ problem27/IntegerArithmetic.java
@@ -25,7 +25,7 @@ public class IntegerArithmetic {
 	}
 	
 	public static boolean isPrime(long n) { 
-		if (n < 0 || n == 0 || n == 1) return false;
+		if (n <= 1) return false;
 		else {
 			for (int i = 2; i * i <= n; i++) {
 				if (n % i == 0) return false;
