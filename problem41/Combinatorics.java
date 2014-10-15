81c211b6dbeeb999e281c0188c18f8a792b7cb3b
family-guy <grking8@gmail.com>
2014-10-15 21:03:52 +0100

    Add Combinatorics.java

--- problem41/Combinatorics.java
+++ problem41/Combinatorics.java
@@ -1,18 +1,17 @@
 import java.util.*;
 /*
  * This class contains combinatorics methods for strings and integers
- * insertCharAt: takes a string and inserts a character (string of length 1) at the given index
- * removeCharAt: takes a string and removes the character at the given index
- * allNewStrings: takes a string (or an array of strings) and a character and returns all new strings that can be made by adding that character
+ * fact: returns the factorial of a non-negative integer
  * permutationsOfAString: returns all permutations of a string
  * showStrings: prints an array of strings
 */
 
+// private methods
+// insertCharAt: takes a string and inserts a character (string of length 1) at the given index
+// removeCharAt: takes a string and removes the character at the given index
+// allNewStrings: takes a string (or an array of strings) and a character and returns all new strings that can be made by adding that character
+
 public class Combinatorics {
-	public static void main(String[] arg) {
-		showStrings(permutationsOfAString("blog"));
-		
-	}
 	private static String insertCharAt(String str, String ch, int i) {		
 		if (i == 0) return ch + str;
 		if (i == str.length()) return str + ch;
@@ -37,6 +36,15 @@ public class Combinatorics {
 		for (int i = 0; i < p * q; i++) {
 			if (i % q == 0) j++;
 			result[i] = allNewStrings(T[j], ch)[i % q];			
+		}
+		return result;
+	}
+	
+	public static int fact(int n) {
+		if (n == 0) return 1;
+		int result = 1;
+		for (int i = 1; i <= n; i++) {
+			result *= i;
 		}
 		return result;
 	}
